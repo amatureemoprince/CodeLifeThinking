85069c992060452a2648cf917c4a8b05800bf05b
代码・生 活・THINKING <2069057986@qq.com>
2025-06-06 19:45:27 +0800

    docs:about🚀

--- docs/.vuepress/notes/about.js
+++ docs/.vuepress/notes/about.js
@@ -8,16 +8,16 @@ export default defineNoteConfig({
       text: '读者必看内容', link: '/about/', icon: 'fluent:important-12-regular',
     },
     {
-      text: 'IT学习', link: '/it-learn/', icon: 'streamline-flex:deepfake-technology-1',
-    },
-    {
-      text: '计算机基础', link: '/cs-basic/', icon: 'icon-park:laptop-computer',
+      text: '计算机基础知识', link: '/cs-basic/', icon: 'icon-park:laptop-computer',
     },
     {
       text: '考研公共科目', link: '/postgraduate/', icon: 'game-icons:graduate-cap',
     },
     {
-      text: '生产力提升工具', link: '/productivity-enhancement-tools/', icon: 'material-icon-theme:folder-tools-open',
+      text: 'Java技术及相关生态', link: '/it-learn/', icon: 'streamline-flex:deepfake-technology-1',
+    },
+    {
+      text: '必备生产工具', link: '/productivity-enhancement-tools/', icon: 'material-icon-theme:folder-tools-open',
     },
     {
       text: '人工智能', link: '/artificial-intelligence/', icon: 'catppuccin:adobe-ai',
